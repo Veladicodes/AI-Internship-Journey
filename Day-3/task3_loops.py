21e82cc1336b42155cd76ceff4cc04709d93ed15
Veladicodes <10403adithya@gmail.com>
2026-08-20 18:49:15 +0530

    Add loops practice and multiplication table script

---
+++ Day-3/task3_loops.py
@@ -0,0 +1,12 @@
+for i in range(1, 101):
+    print(i, end=" ")
+print()
+
+for i in range(1, 51):
+    if i % 2 == 0:
+        print(i, end=" ")
+print()
+
+table_num = 5
+for i in range(1, 11):
+    print(table_num, "x", i, "=", table_num * i)
