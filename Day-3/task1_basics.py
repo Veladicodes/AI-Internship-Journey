6c4007f1bf88cfe0cb9609bf9eeae760cb8569d7
Veladicodes <10403adithya@gmail.com>
2026-08-20 18:49:03 +0530

    Add Python basics and data types practice for Day 3

---
+++ Day-3/task1_basics.py
@@ -0,0 +1,15 @@
+name = "Adithya"
+age = 21
+height = 1.75
+is_student = True
+print("Name:", name, type(name))
+print("Age:", age, type(age))
+print("Height:", height, type(height))
+print("Is Student:", is_student, type(is_student))
+
+user_val = "100"
+converted_val = int(user_val)
+print("Converted to int:", converted_val)
+
+user_val_float = float(converted_val)
+print("Converted to float:", user_val_float)
