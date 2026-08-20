742bb0f540dd19dd810f081732fa0cc796007500
Veladicodes <10403adithya@gmail.com>
2026-08-20 18:49:09 +0530

    Add conditional statements and control flow exercises

---
+++ Day-3/task2_conditionals.py
@@ -0,0 +1,23 @@
+num = 15
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+a = 5
+b = 12
+c = 9
+if a >= b and a >= c:
+    print("Largest:", a)
+elif b >= a and b >= c:
+    print("Largest:", b)
+else:
+    print("Largest:", c)
+
+vote_age = 20
+if vote_age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
